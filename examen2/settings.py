6b030a78d209ffaf4a1b7d7c467d150157c53e0f
JordiUISEK <jordi.sanchez@uisek.edu.ec>
2025-07-06 22:23:37 -0500

    Examen Parcial 2

--- examen2/settings.py
+++ examen2/settings.py
@@ -120,9 +120,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "movies/static",
 ]
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
